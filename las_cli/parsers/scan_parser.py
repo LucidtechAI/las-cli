fec1c6c6f864fb6daf837d8198dd319a15c88510
augustak <august@lucidtech.io>
2018-08-28 13:50:11 +0200

    added support for specifying custom_model and min_confidence

--- las_cli/parsers/scan_parser.py
+++ las_cli/parsers/scan_parser.py
@@ -5,6 +5,23 @@ from functools import partial
 from las import Client, Receipt, Invoice
 
 
+class MissingFileOrUrlException(Exception):
+    pass
+
+
+class InvalidDocumentTypeException(Exception):
+    pass
+
+
+def get_kwargs(args):
+    kwargs = {
+        'minConfidence': args.min_confidence,
+        'customModel': args.custom_model
+    }
+
+    return {k: v for k, v in kwargs.items() if v}
+
+
 def scan(args):
     config = ConfigParser()
     config.read(args.config_path)
@@ -17,18 +34,24 @@ def scan(args):
             receipt = Receipt(filename=args.filename)
         elif args.url:
             receipt = Receipt(url=args.url)
-        response = client.scan_receipt(receipt)
+        else:
+            raise MissingFileOrUrlException('Need to specify url or filename')
+        response = client.scan_receipt(receipt, **get_kwargs(args))
     elif args.document_type == 'invoice':
         if args.filename:
             invoice = Invoice(filename=args.filename)
         elif args.url:
             invoice = Invoice(url=args.url)
-        response = client.scan_invoice(invoice)
+        else:
+            raise MissingFileOrUrlException('Need to specify url or filename')
+        response = client.scan_invoice(invoice, **get_kwargs(args))
+    else:
+        raise InvalidDocumentTypeException()
 
     if response.status_code == 200:
         print(json.dumps(response.detections, indent=2))
     else:
-        print(response.error_message)
+        print(response.requests_response)
 
 
 def scan_parser(subparsers):
@@ -36,6 +59,8 @@ def scan_parser(subparsers):
     parser.add_argument('document_type', choices={'receipt', 'invoice'})
     parser.add_argument('--filename', help='Filename of receipt/invoice image')
     parser.add_argument('--url', help='Url of receipt/invoice image')
+    parser.add_argument('--min_confidence', help='Minimum confidence of scan', type=float)
+    parser.add_argument('--custom_model', help='Optional specify a custom model name')
     parser.add_argument('--profile', default='default',
                         help='Choose profile (~/.lucidtech/las-cli.cfg')
     parser.set_defaults(cmd=scan)
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('README.md') as fp:
 
 setup(
     name='lucidtech-las-cli',
-    version='0.0.7',
+    version='0.0.8',
     description='Command Line Interface for Lucidtech AI Services',
     long_description=readme,
     license='Apache 2.0',
