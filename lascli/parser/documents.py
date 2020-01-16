8de239b654abdea726aa0472a40b9ca4828f2f85
augustak <august@lucidtech.ai>
2020-01-16 13:14:42 +0100

    Added download document and get document

--- lascli/parser/documents.py
+++ lascli/parser/documents.py
@@ -1,7 +1,21 @@
 import filetype
+import logging
 import pathlib
+import base64
 
 from las import Client
+
+
+def get_document(las_client: Client, document_id, download_content):
+    document_resp = las_client.get_document_id(document_id)
+    content = document_resp['content']
+
+    if download_content:
+        logging.info(f'Downloading content to {download_content}')
+        binary = base64.b64decode(content)
+        pathlib.Path(download_content).write_bytes(binary)
+
+    return {**document_resp, 'content': document_resp['content'][:10] + '... [TRUNCATED]'}
 
 
 def get_documents(las_client: Client, batch_id):
@@ -30,6 +44,11 @@ def post_feedback(las_client: Client, document_id, fields):
 def create_documents_parser(subparsers):
     parser = subparsers.add_parser('documents')
     subparsers = parser.add_subparsers()
+
+    get_document_parser = subparsers.add_parser('get')
+    get_document_parser.add_argument('document_id')
+    get_document_parser.add_argument('-d', '--download-content')
+    get_document_parser.set_defaults(cmd=get_document)
 
     list_documents_parser = subparsers.add_parser('list')
     list_documents_parser.add_argument('--batch-id')
