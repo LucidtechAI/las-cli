905fa56d535450d295a62abb9bce39d824e363c4
augustak <august@lucidtech.ai>
2020-01-20 15:16:17 +0100

    Added get user

--- lascli/parser/users.py
+++ lascli/parser/users.py
@@ -1,6 +1,10 @@
 from las import Client
 
 
+def get_user(las_client: Client, user_id):
+    return las_client.get_user_id(user_id)
+
+
 def patch_user(las_client: Client, user_id, consent_hash):
     return las_client.patch_user_id(user_id, consent_hash)
 
@@ -8,6 +12,10 @@ def patch_user(las_client: Client, user_id, consent_hash):
 def create_users_parser(subparsers):
     parser = subparsers.add_parser('users')
     subparsers = parser.add_subparsers()
+
+    get_user_parser = subparsers.add_parser('get')
+    get_user_parser.add_argument('user_id')
+    get_user_parser.set_defaults(cmd=get_user)
 
     update_user_parser = subparsers.add_parser('update')
     update_user_parser.add_argument('user_id')
