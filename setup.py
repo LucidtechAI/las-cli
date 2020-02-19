b13aa2ef86416508a1c1cff745f5f2e66df9688e
augustak <august@lucidtech.ai>
2020-02-19 14:14:06 +0100

    Changed to valid beta version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     },
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
