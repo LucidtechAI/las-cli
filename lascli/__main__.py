dc8966fc7704cce5a779d51bea668e7e4a0c456f
augustak <august@lucidtech.ai>
2020-02-20 12:42:56 +0100

    Added verbose flag

--- lascli/__main__.py
+++ lascli/__main__.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
 import argparse
-import argcomplete
+import configparser
 import inspect
-import logging
 import json
+import logging
+
+import argcomplete
 
 from las import Client, Credentials
-from las.credentials import read_from_file
+from las.credentials import MissingCredentials, read_from_file
 
-from .parser import (create_batches_parser, create_documents_parser, create_users_parser, create_predictions_parser,
-                     create_consents_parser)
+from .parser import (create_batches_parser, create_consents_parser,
+                     create_documents_parser, create_predictions_parser,
+                     create_users_parser)
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--profile')
+    parser.add_argument('--verbose', '-v', action='count', default=0)
     subparsers = parser.add_subparsers()
 
     create_batches_parser(subparsers)
@@ -36,20 +40,30 @@ def args_to_kwargs(args):
         return {}
 
 
+def set_verbosity(verbose):
+    verbosity_levels = [logging.CRITICAL, logging.WARNING, logging.DEBUG]
+    verbosity = verbosity_levels[min(verbose, len(verbosity_levels) - 1)]
+    logging.getLogger().setLevel(verbosity)
+    logging.getLogger('las').setLevel(verbosity)
+
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
+    set_verbosity(args.verbose)
 
-    if args.profile:
-        credentials = Credentials(*read_from_file(section=args.profile))
-        args.las_client = Client(credentials)
-    else:
-        args.las_client = Client()
+    try:
+        if args.profile:
+            credentials = Credentials(*read_from_file(section=args.profile))
+            args.las_client = Client(credentials)
+        else:
+            args.las_client = Client()
+    except (configparser.NoOptionError, configparser.NoSectionError, MissingCredentials) as e:
+        logging.exception(e)
+        print('Could not locate credentials.')
+        return
+
     kwargs = args_to_kwargs(args)
-
-    logging.getLogger().setLevel(logging.INFO)
-    logging.getLogger('las').setLevel(logging.INFO)
-
     if kwargs:
         print(json.dumps(args.cmd(**kwargs), indent=2))
     else:
