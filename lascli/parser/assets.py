e74dee5d9277f3f0a9250d37213e347e3a0c2216
augustak <august@lucidtech.ai>
2020-12-14 16:44:25 +0100

    Update

--- lascli/parser/assets.py
+++ lascli/parser/assets.py
@@ -61,7 +61,7 @@ def create_assets_parser(subparsers):
 
     update_asset_parser = subparsers.add_parser('update')
     update_asset_parser.add_argument('asset_id')
-    update_asset_parser.add_argument('--asset-path')
+    update_asset_parser.add_argument('--asset-path', default=NoValue)
     update_asset_parser.add_argument('--name', type=nullable, default=NoValue)
     update_asset_parser.add_argument('--description', type=nullable, default=NoValue)
     update_asset_parser.set_defaults(cmd=update_asset)
