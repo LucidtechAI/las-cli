cab4e839f623f2926c5d52329e6d7f8b4ad08cb4
augustak <august@lucidtech.ai>
2021-01-21 16:32:36 +0100

    Added user update

--- lascli/parser/users.py
+++ lascli/parser/users.py
@@ -1,4 +1,9 @@
+import base64
+import pathlib
+
 from las import Client
+
+from lascli.util import nullable, NotProvided
 
 
 def list_users(las_client: Client, max_results, next_token):
@@ -13,6 +18,13 @@ def create_user(las_client: Client, email):
     return las_client.create_user(email)
 
 
+def update_user(las_client: Client, user_id, **optional_args):
+    if avatar := optional_args.get('avatar'):
+        optional_args['avatar'] = base64.b64encode(pathlib.Path(avatar).read_bytes()).decode()
+
+    return las_client.update_user(user_id, **optional_args)
+
+
 def delete_user(las_client: Client, user_id):
     return las_client.delete_user(user_id)
 
@@ -24,6 +36,17 @@ def create_users_parser(subparsers):
     create_user_parser = subparsers.add_parser('create')
     create_user_parser.add_argument('email')
     create_user_parser.set_defaults(cmd=create_user)
+
+    update_user_parser = subparsers.add_parser('update')
+    update_user_parser.add_argument('user_id')
+    update_user_parser.add_argument('--name', type=nullable, default=NotProvided)
+    update_user_parser.add_argument(
+        '--avatar',
+        help='Path to avatar JPEG image or "null" to remove avatar from user.',
+        type=nullable,
+        default=NotProvided
+    )
+    update_user_parser.set_defaults(cmd=update_user)
 
     list_users_parser = subparsers.add_parser('list')
     list_users_parser.add_argument('--max-results', '-m', type=int, default=None)
