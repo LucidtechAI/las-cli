1398c6cf15670f888972204555b04cc36091cccd
Magnus Rud <mag.aars@gmail.com>
2021-02-16 13:45:06 +0100

    add description to delete parser

--- lascli/parser/transitions.py
+++ lascli/parser/transitions.py
@@ -110,7 +110,7 @@ def create_transitions_parser(subparsers):
     execute_parser.add_argument('transition_id')
     execute_parser.set_defaults(cmd=execute_transition)
 
-    delete_parser = subparsers.add_parser('delete')
+    delete_parser = subparsers.add_parser('delete', description='Will fail if transition is in use by one or more workflows')
     delete_parser.add_argument('transition_id')
     delete_parser.set_defaults(cmd=delete_transition)
 
