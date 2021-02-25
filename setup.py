b7c3fa8a857a93b14f0ec3677eb3e4413de1704f
Magnus Rud <mag.aars@gmail.com>
2021-02-25 15:41:57 +0100

    add version check and update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as fp:
 
 setup(
     name='lucidtech-las-cli',
-    version='2.2.6',
+    version='2.2.7',
     description='CLI for Lucidtech AI Services',
     long_description=readme,
     license='Apache 2.0',
