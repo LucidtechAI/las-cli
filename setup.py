406e2f5bce8363825f14fb1f1f13ae4db0f2776d
Magnus <mag.aars@gmail.com>
2021-03-29 11:25:05 +0200

    bugfix (#25)
    * bugfix
    
    * update version
    
    * fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as fp:
 
 setup(
     name='lucidtech-las-cli',
-    version='2.2.7',
+    version='2.2.8',
     description='CLI for Lucidtech AI Services',
     long_description=readme,
     license='Apache 2.0',
