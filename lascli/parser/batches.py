b56246a7f67e10b369247828c4684bd7a2cb0390
Magnus Rud <mag.aars@gmail.com>
2021-05-12 11:14:09 +0200

    update model, batch, app client and get model

--- lascli/parser/batches.py
+++ lascli/parser/batches.py
@@ -1,5 +1,7 @@
 from las import Client
 
+from lascli.util import nullable, NotProvided
+
 
 def post_batches(las_client: Client, **optional_args):
     return las_client.create_batch(**optional_args)
@@ -7,6 +9,10 @@ def post_batches(las_client: Client, **optional_args):
 
 def list_batches(las_client: Client, max_results=None, next_token=None):
     return las_client.list_batches(max_results=max_results, next_token=next_token)
+
+
+def update_batch(las_client: Client, batch_id, **optional_args):
+    return las_client.update_batch(batch_id, **optional_args)
 
 
 def delete_batch(las_client: Client, batch_id, delete_documents):
@@ -26,6 +32,12 @@ def create_batches_parser(subparsers):
     list_batches_parser.add_argument('--max-results', '-m', type=int, default=None)
     list_batches_parser.add_argument('--next-token', '-n', default=None)
     list_batches_parser.set_defaults(cmd=list_batches)
+
+    update_batch_parser = subparsers.add_parser('update')
+    update_batch_parser.add_argument('batch_id')
+    update_batch_parser.add_argument('--name', type=nullable, default=NotProvided)
+    update_batch_parser.add_argument('--description', type=nullable, default=NotProvided)
+    update_batch_parser.set_defaults(cmd=update_batch)
 
     delete_batch_parser = subparsers.add_parser('delete')
     delete_batch_parser.add_argument('batch_id')
--- lascli/parser/models.py
+++ lascli/parser/models.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from las import Client
 
+from lascli.util import NotProvided, nullable
+
 
 def create_model(
     las_client: Client,
@@ -27,6 +29,29 @@ def list_models(las_client: Client, max_results, next_token):
     return las_client.list_models(max_results=max_results, next_token=next_token)
 
 
+def update_model(
+    las_client: Client,
+    width=None,
+    height=None,
+    field_config_path=None,
+    preprocess_config_path=None,
+    start_training=False,
+    **optional_args,
+):
+    field_config = json.loads(Path(field_config_path).read_text()) if field_config_path else None
+    preprocess_config = json.loads(Path(preprocess_config_path).read_text()) if preprocess_config_path else None
+    status = 'training' if start_training else None
+
+    return las_client.update_model(
+        width=width,
+        height=height,
+        field_config=field_config,
+        preprocess_config=preprocess_config,
+        status=status,
+        **optional_args,
+    )
+
+
 def create_models_parser(subparsers):
     parser = subparsers.add_parser('models')
     subparsers = parser.add_subparsers()
@@ -40,9 +65,23 @@ def create_models_parser(subparsers):
     create_parser.add_argument('--description')
     create_parser.set_defaults(cmd=create_model)
 
-    list_models_parser = subparsers.add_parser('list')
-    list_models_parser.add_argument('--max-results', '-m', type=int, default=None)
-    list_models_parser.add_argument('--next-token', '-n', type=str, default=None)
-    list_models_parser.set_defaults(cmd=list_models)
+    update_parser = subparsers.add_parser('update')
+    update_parser.add_argument(
+        '--field-config-path',
+        '-f',
+        help='configuration of the fields that the model will predict',
+    )
+    update_parser.add_argument('--width', type=int)
+    update_parser.add_argument('--height', type=int)
+    update_parser.add_argument('--start-training', action='store_true', default=False)
+    update_parser.add_argument('--preprocess-config-path', '-p', help='configuration of the step before the prediction')
+    update_parser.add_argument('--name', type=nullable, default=NotProvided)
+    update_parser.add_argument('--description', type=nullable, default=NotProvided)
+    update_parser.set_defaults(cmd=update_model)
+
+    list_parser = subparsers.add_parser('list')
+    list_parser.add_argument('--max-results', '-m', type=int, default=None)
+    list_parser.add_argument('--next-token', '-n', type=str, default=None)
+    list_parser.set_defaults(cmd=list_models)
 
     return parser
