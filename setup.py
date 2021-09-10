e76ea311fce1a6e63369213acc89b743debaa0c4
August André Kvernmo <august@lucidtech.ai>
2021-09-10 10:18:51 +0200

    Update Python SDK, no longer need API key (#44)
    * Update Python SDK, no longer need API key
    
    * Updated changelog

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as fp:
 
 setup(
     name='lucidtech-las-cli',
-    version='3.1.1',
+    version='4.0.0',
     description='CLI for Lucidtech AI Services',
     long_description=readme,
     license='Apache 2.0',
