5bdf84adce45dbe7d9a9b735e92d8dd5bf711642
Magnus Rud <mag.aars@gmail.com>
2022-02-23 10:06:04 +0100

    add completed config to workflows test

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -2,26 +2,30 @@ import pytest
 from tests import service, util
 
 
+@pytest.mark.parametrize('completed_config', [('--completed-config', str(util.transition_parameters_path())), ()])
 @pytest.mark.parametrize('error_config', [('--error-config', str(util.error_config_path())), ()])
-def test_workflows_create(parser, client, error_config, name_and_description):
+def test_workflows_create(parser, client, name_and_description, error_config, completed_config):
     args = [
         'workflows',
         'create',
         str(util.assets_folder() / 'workflow_specification.json'),
         *name_and_description,
         *error_config,
+        *completed_config,
     ]
     util.main_parser(parser, client, args)
 
 
+@pytest.mark.parametrize('completed_config', [('--completed-config', str(util.transition_parameters_path())), ()])
 @pytest.mark.parametrize('error_config', [('--error-config', str(util.error_config_path())), ()])
-def test_workflows_update(parser, client, name_and_description, error_config):
+def test_workflows_update(parser, client, name_and_description, error_config, completed_config):
     args = [
         'workflows',
         'update',
         service.create_workflow_id(),
         *name_and_description,
         *error_config,
+        *completed_config,
     ]
 
     if len(args) == 3: # patch call requires at least one change
