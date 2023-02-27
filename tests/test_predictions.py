7c4d9d61826e692e113046d6f18f60565837a102
Magnus <mag.aars@gmail.com>
2023-02-27 14:22:59 +0100

    Add --rotation to predictions create (#94)
    * add --rotation to predictions create
    
    * update tests
    
    * dont set default values of parameters that could conflict with the preprocessing config from the model
    
    * fixes
    
    * update version

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -16,6 +16,7 @@ def test_predictions_list(parser, client, list_defaults, sort_by, order):
     util.main_parser(parser, client, args)
 
 
+@pytest.mark.parametrize('rotation', [['--rotation', '90'], []])
 @pytest.mark.parametrize('auto_rotate', [['--auto-rotate'], []])
 @pytest.mark.parametrize('max_pages', [['--max-pages', '3'], []])
 @pytest.mark.parametrize('image_quality', [['--image-quality', 'HIGH'], ['--image-quality', 'LOW'], []])
@@ -23,15 +24,16 @@ def test_predictions_list(parser, client, list_defaults, sort_by, order):
     ['--postprocess-config', json.dumps({'strategy': 'BEST_FIRST'})],
     [],
 ])
-def test_predictions_get(parser, client, auto_rotate, max_pages, image_quality, postprocess_config):
+def test_predictions_create(parser, client, auto_rotate, max_pages, image_quality, postprocess_config, rotation):
     args = [
         'predictions',
         'create',
         service.create_document_id(),
         service.create_model_id(),
         *auto_rotate,
-        *max_pages,
         *image_quality,
+        *max_pages,
         *postprocess_config,
+        *rotation,
     ]
     util.main_parser(parser, client, args)
