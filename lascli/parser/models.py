c3b404aac8740895ba26ed4489b2b765c15160b9
augustak <august@lucidtech.ai>
2023-12-01 13:19:59 +0100

    Updates

--- lascli/parser/models.py
+++ lascli/parser/models.py
@@ -101,16 +101,16 @@ def create_models_parser(subparsers):
     create_parser.add_argument('--preprocess-config', '-p', type=json_or_json_path, help=textwrap.dedent('''
         Path or inline JSON with the pre processing configuration for predictions made by this model
         {
-            'autoRotate': True | False                          (optional)
-            'maxPages': 1 - 3                                   (optional)
-            'imageQuality': 'LOW' | 'HIGH'                      (optional)
-            'pages': List with up to 3 page-indices to process  (optional)
-            'rotation': 0, 90, 180 or 270                       (optional)
+            "autoRotate": True | False                          (optional)
+            "maxPages": 1 - 3                                   (optional)
+            "imageQuality": "LOW" | "HIGH"                      (optional)
+            "pages": List with up to 3 page-indices to process  (optional)
+            "rotation": 0, 90, 180 or 270                       (optional)
         }
         Examples:
-        {'pages': [0, 1, 5], 'autoRotate': True}
-        {'pages': [0, 1, -1], 'rotation': 90, 'imageQuality': 'HIGH'}
-        {'maxPages': 3, 'imageQuality': 'LOW'}
+        {"pages": [0, 1, 5], "autoRotate": True}
+        {"pages": [0, 1, -1], "rotation": 90, "imageQuality": "HIGH"}
+        {"maxPages": 3, "imageQuality": "LOW"}
     '''))
     create_parser.add_argument('--postprocess-config', type=json_or_json_path, help=textwrap.dedent('''
         Path or inline JSON with the post processing configuration for predictions made by this model
@@ -122,7 +122,7 @@ def create_models_parser(subparsers):
             }
         }
         Examples:
-        {"strategy": "BEST_FIRST"}
+        {"strategy": "BEST_FIRST", "outputFormat": "v2"}
         {"strategy": "BEST_N_PAGES", "parameters": {"n": 3}}
         {"strategy": "BEST_N_PAGES", "parameters": {"n": 3, "collapse": true}}
     '''))
@@ -167,16 +167,16 @@ def create_models_parser(subparsers):
     update_parser.add_argument('--preprocess-config', type=json_or_json_path, help=textwrap.dedent('''
         Path or inline JSON with the pre processing configuration for predictions made by this model
         {
-            'autoRotate': True | False                          (optional)
-            'maxPages': 1 - 3                                   (optional)
-            'imageQuality': 'LOW' | 'HIGH'                      (optional)
-            'pages': List with up to 3 page-indices to process  (optional)
-            'rotation': 0, 90, 180 or 270                       (optional)
+            "autoRotate": True | False                          (optional)
+            "maxPages": 1 - 3                                   (optional)
+            "imageQuality": "LOW" | "HIGH"                      (optional)
+            "pages": List with up to 3 page-indices to process  (optional)
+            "rotation": 0, 90, 180 or 270                       (optional)
         }
         Examples:
-        {'pages': [0, 1, 5], 'autoRotate': True}
-        {'pages': [0, 1, -1], 'rotation': 90, 'imageQuality': 'HIGH'}
-        {'maxPages': 3, 'imageQuality': 'LOW'}
+        {"pages": [0, 1, 5], "autoRotate": True}
+        {"pages": [0, 1, -1], "rotation": 90, "imageQuality": "HIGH"}
+        {"maxPages": 3, "imageQuality": "LOW"}
     '''))
     update_parser.add_argument('--postprocess-config', type=json_or_json_path, help=textwrap.dedent('''
         Path or inline JSON with the post processing configuration for predictions made by this model
@@ -188,7 +188,7 @@ def create_models_parser(subparsers):
             }
         }
         Examples:
-        {"strategy": "BEST_FIRST"}
+        {"strategy": "BEST_FIRST", "outputFormat": "v2"}
         {"strategy": "BEST_N_PAGES", "parameters": {"n": 3}}
         {"strategy": "BEST_N_PAGES", "parameters": {"n": 3, "collapse": true}}
     '''))
